453b7de9667f5becfd3d71bc8dac9ad9e329bfc0
auter <auterskym@gmail.com>
2018-11-21 15:24:57 +0100

    - Added documentation to CnbRatesClient and to respective method in MyService

--- SOAPWebService/CnbRatesClient.cs
+++ SOAPWebService/CnbRatesClient.cs
@@ -8,9 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SOAPWebService {
+    /// <summary>
+    /// Třída pro získávání kurzu CZK vůči dalším měnám
+    /// </summary>
     public static class CnbRatesClient {
+        //URL pro API České národní banky
         private const string API_URL = @"http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt";
 
+        /// <summary>
+        /// Metoda pro asynchronní získání kurzů
+        /// </summary>
+        /// <returns>Datová tabulka kurzů</returns>
         public static async Task<DataTable> GetRatesAsync() {
             using (var wc = new WebClient()) {
                 wc.Encoding = Encoding.UTF8;
@@ -23,40 +31,50 @@ namespace SOAPWebService {
             }
         }
 
+        /// <summary>
+        /// Metoda pro získání datové tabulky z textového výstupu API České národní banky
+        /// </summary>
+        /// <param name="rateString">Textový soubor s kurzy</param>
+        /// <returns>Datová tabulka z vybranými kurzy</returns>
         private static DataTable Parse(string rateString) {
-            var rates = new DataTable();
+            var rates = new DataTable();                            //Vytvoření datové tabulky
 
-            rates.Columns.Add("Code", typeof(string));
+            rates.Columns.Add("Code", typeof(string));              //Přidání sloupců
             rates.Columns.Add("Rate", typeof(decimal));
 
-            rates.Rows.Add("CZK", 1);
+            rates.Rows.Add("CZK", 1);                               //Záznam pro CZK, převod CZK - CZK má kurz 1
 
-            foreach (var line in rateString.SplitLines()) {
-                var data = line.Split('|');
-                var code = data[3];
-                var rate = data[4];
-                if (code == "EUR" || code == "USD" || code == "GBP") {
-                    rates.Rows.Add(code, decimal.Parse(rate, CultureInfo.GetCultureInfo("cs-CZ")));
+            foreach (var line in rateString.SplitLines()) {         //Volání řádků po jednom
+                var data = line.Split('|');                         //Rozdělení rádku
+                var code = data[3];                                 //Získání kódu měny
+                var rate = data[4];                                 //Získání kurzu
+                if (code == "EUR" || code == "USD" || code == "GBP") {  //Vyběr měn
+                    rates.Rows.Add(code, decimal.Parse(rate, CultureInfo.GetCultureInfo("cs-CZ")));     //Přidání záznamu do tabulky
                 }
             }
 
             return rates;
         }
 
+        /// <summary>
+        /// Pomocná metoda pro načítání řádků, postupně iteruje přes řádky a vrací jeden za druhým
+        /// </summary>
+        /// <param name="input">Text s kurzy</param>
+        /// <returns>Řádek z textu</returns>
         private static IEnumerable<string> SplitLines(this string input) {
-            if (input == null) {
+            if (input == null) {                                    //Vstup je prázdný, přerušuje se iterace
                 yield break;
             }
 
-            using (var reader = new StringReader(input)) {
+            using (var reader = new StringReader(input)) {          //K procházení textu je použit StringReader
                 string line;
                 var i = 1;
-                while ((line = reader.ReadLine()) != null) {
-                    if (!(i > 2)) {
+                while ((line = reader.ReadLine()) != null) {        //Iterace dokud zbývá text
+                    if (!(i > 2)) {                                 //Přeskočení hlavičky
                         i++;
                         continue;
                     }
-                    yield return line;
+                    yield return line;                              //Vrácení řádku
                 }
             }
         }
--- SOAPWebService/MyService.cs
+++ SOAPWebService/MyService.cs
@@ -28,32 +28,39 @@ namespace SOAPWebService {
             return new Person(name, position);
         }
 
+        /// <summary>
+        /// Metoda pro výpočet kurzu
+        /// </summary>
+        /// <param name="fromCurr">Vstupní měna</param>
+        /// <param name="toCurr">Výstupní měna</param>
+        /// <param name="amount">Množství k převodu</param>
+        /// <returns>Převedené množství dle kurzu</returns>
         public async Task<decimal> GetExchange(string fromCurr, string toCurr, int amount) {
-            var rates = await CnbRatesClient.GetRatesAsync();
-            if (rates == null) //Nepodaří se získat kurzy
+            var rates = await CnbRatesClient.GetRatesAsync();                                               //Získání tabulky kurzů
+            if (rates == null)                                                                              //Nepodaří se získat kurzy
                 throw new FaultException<OperationCanceledException>(new OperationCanceledException(),
                             new FaultReason("Službě se nepodařilo získat data pro konverzi měn"));
 
-            fromCurr = fromCurr.ToUpper();
+            fromCurr = fromCurr.ToUpper();                                                                  //Převedení na velká písmena
             toCurr = toCurr.ToUpper();
 
             decimal result = 0;
             if (toCurr == "CZK" && (fromCurr == "EUR" || fromCurr == "USD" || fromCurr == "GBP" || fromCurr == "CZK")) { //Do CZK
-                var rate = (from r in rates.AsEnumerable()
+                var rate = (from r in rates.AsEnumerable()                              //Získání kurzu
                             where r.Field<string>("Code").Equals(fromCurr)
                             select r.Field<decimal>("Rate")).First();
-                result = rate * amount;
+                result = rate * amount;                                                 //Výpočet hodnoty
             } else if (fromCurr == "CZK" && (toCurr == "EUR" || toCurr == "USD" || toCurr == "GBP" || toCurr == "CZK")) { //Z CZK
-                var rate = 1 / (from r in rates.AsEnumerable()
+                var rate = 1 / (from r in rates.AsEnumerable()                          //Získání hodnoty 1 / kurz
                                 where r.Field<string>("Code").Equals(toCurr)
                                 select r.Field<decimal>("Rate")).First();
-                result = rate * amount;
+                result = rate * amount;                                                 //Výpočet hodnoty
             } else {
-                throw new FaultException<NotImplementedException>(new NotImplementedException(), 
+                throw new FaultException<NotImplementedException>(new NotImplementedException(),            //Ošetření vstupů
                             new FaultReason("Služba umí převádět pouze z CZK do EUR, USD, GBP a naopak"));
             }
 
-            return decimal.Round(result, 2, MidpointRounding.AwayFromZero);
+            return decimal.Round(result, 2, MidpointRounding.AwayFromZero);             //Návrat vypočítané hodnoty, na dvě místa
         }
     }
 }
